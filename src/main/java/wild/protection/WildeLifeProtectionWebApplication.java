05968a6ad73bf40d33c6320e2b6a2387a494f603
joel <madushanjoel@.com>
2023-11-27 00:24:59 +0530

    delete comment

--- src/main/java/wild/protection/WildeLifeProtectionWebApplication.java
+++ src/main/java/wild/protection/WildeLifeProtectionWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WildeLifeProtectionWebApplication {
-//chech
+
 	public static void main(String[] args) {
 		SpringApplication.run(WildeLifeProtectionWebApplication.class, args);
 	}
